33cb0f703f85dec752d1a62af6ffbedf3ac38625
kyfan <kyfan@email>
2024-01-07 02:44:22 +0800

    update nvim folder

--- update_config.sh
+++ update_config.sh
@@ -6,12 +6,13 @@ link_conf ()
 {
 	src=$1
 	dst=$2
-	echo "linking $src -> $dst"
+	echo "linking $src as $dst"
 	ln -f "$src" "$dst"
 }
 
 echo "updating configs"
 link_conf ./tmux/.tmux.conf ~/.tmux.conf
 link_conf ./fish/config.fish ~/.config/fish/config.fish
-link_conf ./nvim/init.lua ~/.config/nvim/init.lua
+echo "linking $(pwd)/nvim as ~/.config/nvim"
+ln -s $(pwd)/nvim ~/.config/nvim
 echo "all configs are updated"
