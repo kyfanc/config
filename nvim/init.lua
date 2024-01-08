0129d664514b49c5f1084765dd5f7c4e690c4202
kyfan <kyfan@email>
2024-01-08 13:04:46 +0800

    <leader>fg for live grep

--- nvim/init.lua
+++ nvim/init.lua
@@ -267,6 +267,7 @@ require('lazy').setup({
   'tpope/vim-surround',
   'tpope/vim-abolish',
 
+  'vim-test/vim-test',
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -444,6 +445,7 @@ vim.keymap.set('n', '<leader>ff', function()
     previewer = false,
   })
 end, { desc = '[f]ind [f]uzzily search in current buffer' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[f]ind by [G]rep' })
 
 local function telescope_live_grep_open_files()
   require('telescope.builtin').live_grep {
@@ -458,7 +460,6 @@ vim.keymap.set('n', '<leader>p', require('telescope.builtin').find_files, { desc
 -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 -- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 -- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
--- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 -- vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 -- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 -- vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
