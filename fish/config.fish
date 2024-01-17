860b0312ac5447814a2a884731bb0565cc870d6c
kyfan <kyfan@email>
2024-01-18 00:44:28 +0800

    add helix config and paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 # make homebrew installed app discoverable
 fish_add_path /opt/homebrew/bin
+fish_add_path ~/go/bin
+fish_add_path ~/Library/Python/3.9/bin
+fish_add_path ~/.cargo/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
