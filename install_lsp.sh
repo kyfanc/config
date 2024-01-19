ef2cd3c0c012f53c7c688b019a5559b53c96da8c
kyfan <kyfan@email>
2024-01-19 21:52:25 +0800

    add lsp installation script

---
+++ install_lsp.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# terraform
+brew install hashicorp/tap/terraform-ls
+
+# markdown
+brew install marksman
+
+# ts/js
+npm install -g typescript-language-server typescript
+
+# bash
+npm install -g bash-language-server
+
