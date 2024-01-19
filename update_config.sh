bc9d2f46705869313b8ea4bc041a8042636e728e
kyfan <kyfan@email>
2024-01-19 22:00:02 +0800

    add helix config update script

--- update_config.sh
+++ update_config.sh
@@ -15,4 +15,6 @@ link_conf ./tmux/.tmux.conf ~/.tmux.conf
 link_conf ./fish/config.fish ~/.config/fish/config.fish
 echo "linking $(pwd)/nvim as ~/.config/nvim"
 ln -sf $(pwd)/nvim ~/.config/nvim
+echo "linking $(pwd)/helix as ~/.config/helix"
+ln -sf $(pwd)/helix ~/.config/helix
 echo "all configs are updated"
