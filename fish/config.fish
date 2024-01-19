9b41f0b646ebd494de5140a5a0cfa02c2d07c51d
kyfan <kyfan@email>
2024-01-19 21:58:00 +0800

    editor shortcut

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,13 @@
-# make homebrew installed app discoverable
+# make package manager installed app discoverable
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/go/bin
 fish_add_path ~/Library/Python/3.9/bin
 fish_add_path ~/.cargo/bin
+
+# editor
+set -U EDITOR "hx"
+# short cut
+alias e="$EDITOR"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
