64dd2220ecf35329176b8e4f89779cf892d3c930
kyfan <kyfan@email>
2024-01-27 11:38:42 +0800

    [fish] use vi binding for fish cmd edit

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ fish_add_path ~/go/bin
 fish_add_path ~/Library/Python/3.9/bin
 fish_add_path ~/.cargo/bin
 
+# use vi binding for command editing
+set -g fish_key_bindings fish_vi_key_bindings
+
 # editor
 set -U EDITOR "hx"
 # short cut
