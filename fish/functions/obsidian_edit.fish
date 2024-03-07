ee3cad6cfd5d086739a3cc37b264404f21b78862
kyfanc <chrisfan918@gmail.com>
2024-03-07 10:54:30 +0800

    [fish] included functions, fix editors in shell

---
+++ fish/functions/obsidian_edit.fish
@@ -0,0 +1,4 @@
+function obsidian_edit --wraps=hx\ -w\ \'/Users/kyfan/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents\' --description alias\ obsidian_edit=hx\ -w\ \'/Users/kyfan/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents\'
+  hx -w '/Users/kyfan/Library/Mobile Documents/iCloud~md~obsidian/Documents' $argv
+        
+end
