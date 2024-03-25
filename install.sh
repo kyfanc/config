fb2841b7fbf9a140f6dc19627dd3c43798f6e329
kyfanc <chrisfan918@gmail.com>
2024-03-25 16:54:10 +0800

    [brew] use brewfile

--- install.sh
+++ install.sh
@@ -8,27 +8,10 @@ if [ ! $(which brew) ]; then
 	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-ensure_brew_installed() {
-	app=$1
-	if [ ! $(which "$app") ]; then
-		echo "installing $app..."
-		brew install $app
-	fi
-	echo "$app is installed"
-}
-
-ensure_brew_installed fish
-ensure_brew_installed zellij
-ensure_brew_installed helix
-ensure_brew_installed starship
-ensure_brew_installed tree
-ensure_brew_installed htop
-ensure_brew_installed git-delta
-ensure_brew_installed bat
-# for recording terminal
-ensure_brew_installed asciinema
-# for converting recording to gif
-ensure_brew_installed agg
+#
+# install brew packages
+#
+brew bundle install --file=brewfile
  
 #
 # zellij plugins
@@ -48,18 +31,17 @@ bash ./update_config.sh
 #
 echo "configuring fish"
 chsh -s $(which fish)
+echo $(which fish) >> /etc/shells
 echo "set fish as default shell"
 echo "fish is ready"
-
-echo "installing font"
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code-nerd-font
-ehco "!! ACTION_ITEM: import custom.iterm.json profile to iterm2"
 
 #
 #  screenshots
 mkdir -p ~/Screenshots
 defaults write com.apple.screencapture ~/Screenshots
+
+ehco "!! ACTION_ITEMS:"
+echo "- import custom.iterm.json profile to iterm2"
 
 echo "done"
 
