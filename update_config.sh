9a2298bbabe40b26b762eed16a862b7f3bd720a8
kyfanc <chrisfan918@gmail.com>
2025-01-03 12:00:17 +0800

    [ghostty] introduce

--- update_config.sh
+++ update_config.sh
@@ -27,6 +27,7 @@ link_file $(pwd)/fish/config.fish ~/.config/fish/config.fish
 link_folder $(pwd)/fish/functions ~/.config/fish/
 link_file $(pwd)/starship/starship.toml ~/.config/starship.toml
 link_folder $(pwd)/alacritty ~/.config/
+link_folder $(pwd)/ghostty ~/.config/
 link_folder $(pwd)/helix ~/.config/
 link_folder $(pwd)/zellij ~/.config/
 link_folder $(pwd)/gitui ~/.config/
