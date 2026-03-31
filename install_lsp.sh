ce269e34c99d6c1f34b31827ce5999757561d70c
kyfanc <chrisfan918@gmail.com>
2026-04-01 00:49:00 +0800

    [general] install yaml-language-server via npm

--- install_lsp.sh
+++ install_lsp.sh
@@ -1,14 +1,10 @@
 #/bin/bash
 
-# terraform
-brew install hashicorp/tap/terraform-ls
-
-# markdown
-brew install marksman
-
 # ts/js
 npm install -g typescript-language-server typescript
 
 # bash
 npm install -g bash-language-server
 
+# yaml
+npm install -g yaml-language-server
